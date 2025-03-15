a7c6beac45f1ecced47e36e0ed8db8fbf72e4fbc
Edson Petry <edsonlbpetry@gmail.com>
2025-03-15 14:46:29 -0400

    completed project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Web Development Project 2 - CS Trivia
+# Web Development Project 3 - CS Trivia
 
 Submitted by: **Edson Petry**
 
@@ -8,13 +8,20 @@ Time spent: **2** hours spent in total
 
 ## Required Features
 
+## Required Features
+
 The following **required** functionality is completed:
 
-- [x] **The title of the card set and some information about it, such as a short description and the total number of cards are displayed**
-- [x] **A single card at a time is displayed, only showing one of the components of the information pair**
-- [x] **A list of card pairs is created**
-- [x] **Clicking on the card shows the corresponding component of the information pair**
-- [x] **Clicking the next button displays a random new card**
+- [x] **The user can enter their guess in a box before seeing the flipside of the card**
+- [x] **Clicking on a submit button shows visual feedback about whether the answer was correct or incorrect**
+- [x] **A back button is displayed on the card and can be used to return to the previous card in a set sequence**
+- [x] **A next button is displayed on the card and can be used to navigate to the next card in a set sequence**
+
+The following **optional** features are implemented:
+
+- [x] A shuffle button is used to randomize the order of the cards
+- [x] A user's answer may be counted as correct even when it is slightly different from the target answer
+
 
 ## Video Walkthrough
 
@@ -26,8 +33,6 @@ Here's a walkthrough of implemented required features:
 GIF created with [peek](https://github.com/phw/peek).
 
 ## License
-
-    Copyright [yyyy] [name of copyright owner]
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import FlashCard from './components/FlashCard'
-import './App.css'
+import { useState, useEffect } from 'react';
+import FlashCard from './components/FlashCard';
+import './App.css';
 
 const App = ({ questions }) => {
-  const [currentCard, setCurrentCard] = useState(questions[0])
-  
+  const [cards, setCards] = useState(questions);
+  const [currentCard, setCurrentCard] = useState(cards[0]);
+  const [userGuess, setUserGuess] = useState("");
+
+  useEffect(() => {
+    shuffleCards(cards);
+  }, []);
+
+  const shuffleCards = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    // reassign ids
+    for (let i = array.length -1; i > 0; i--){
+      array[i].id = i;
+    }
+
+    setCards([...array]);
+    setCurrentCard(cards[0])
+  }
+
+  const handleShuffle = () => {
+    shuffleCards(cards);
+  }
+
   const handleNext = () => {
-    if (currentCard.id !== questions.length-1){
-      setCurrentCard(questions[currentCard.id + 1])
+    if (currentCard.id !== cards.length - 1) {
+      setCurrentCard(cards[currentCard.id + 1]);
     }
   }
 
   const handlePrev = () => {
-    if (currentCard.id !== 0){
-      setCurrentCard(questions[currentCard.id - 1])
+    if (currentCard.id !== 0) {
+      setCurrentCard(cards[currentCard.id - 1]);
     }
+  }
+
+
+  const checkGuess = (event) => {
+    event.preventDefault();
+    if (currentCard.answer.includes(userGuess)){
+      alert("Correct!");
+    } else {
+      alert("Wrong!");
+    }
+
+    setUserGuess("");
+  }
+
+  const handleUserGuess = (event) => {
+    setUserGuess(event.target.value);
   }
 
   return (
     <>
       <h1>CS Trivia!</h1>
       <h2>Test your CS and SWE knowledge</h2>
-      <h3>{questions.length} cards loaded</h3>
-      <FlashCard question={currentCard.question} answer={currentCard.answer}/>
-
+      <h3>{cards.length} cards loaded</h3>
+      <FlashCard question={currentCard.question} answer={currentCard.answer} />
+      <form onSubmit={checkGuess}>
+        <input 
+          value={userGuess} 
+          onChange={handleUserGuess}
+        />
+        <button type="submit">Check Guess</button>
+      </form>
       <div>
+        <button className="shuffleButton" onClick={handleShuffle}>Shuffle</button>
         <button className="prevButton" onClick={handlePrev}>Prev</button>
         <button className="nextButton" onClick={handleNext}>Next</button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
